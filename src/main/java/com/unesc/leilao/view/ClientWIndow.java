48ed0e075da92dbf597e47e97f1f1f89cbbc92ea
Matheus Dimer <matheus-delima@outlook.com>
2023-11-11 12:07:58 -0300

    Tela inicial do cliente.

---
+++ src/main/java/com/unesc/leilao/view/ClientWIndow.java
@@ -0,0 +1,14 @@
+package com.unesc.leilao.view;
+
+import javax.swing.*;
+
+public class ClientWIndow {
+    private JTextField usernameTextField;
+    private JButton entrarButton;
+    private JTable table1;
+    private JTextArea logArea;
+    private JPanel panel;
+    private JComboBox produtosCombo;
+    private JTextField valorField;
+    private JButton lanceButton;
+}
